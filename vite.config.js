fbaedae8c483eca6e65a3a1fdf65b96b193a00c6
= <=>
2025-07-23 11:19:55 +0530

    vite-config update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WorldAtlas/',
-});
+  base: '/WorldAtlas/', // <-- This must match your repo name exactly!
+})
